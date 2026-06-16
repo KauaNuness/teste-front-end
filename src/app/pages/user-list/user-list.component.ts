3696148d8668ddbc231c43fcc309c39e8022bef9
kauanuness <kaua.souza@sptech.school>
2026-06-16 17:25:51 -0300

    feat(user-service): integrate backend with user list and table rendering

---
+++ src/app/pages/user-list/user-list.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user.model';
+
+@Component({
+  selector: 'app-user-list',
+  standalone: true,
+  imports: [CommonModule, MatTableModule, MatButtonModule],
+  templateUrl: './user-list.component.html',
+  styleUrl: './user-list.component.css'
+})
+export class UserListComponent implements OnInit {
+
+  private readonly userService = inject(UserService);
+
+  users: User[] = [];
+
+  displayedColumns: string[] = ['id', 'name', 'email', 'phone', 'actions'];
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers(): void {
+    this.userService.findAll().subscribe({
+      next: (data) => {
+        console.log('DADOS RECEBIDOS:', data);
+        this.users = data;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
+
+  deleteUser(id: number): void {
+    this.userService.delete(id).subscribe({
+      next: () => this.loadUsers(),
+      error: (err) => console.error(err)
+    });
+  }
+}
